20a34a56e41f8927fd58fb6493a6c4e01fbdd093
Felix Kollmann <fk@konsorten.de>
2018-09-04 13:25:31 +0200

    Entrypoint now handles failed processings

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,21 @@ if [ ! -x "$BIN" ]; then
     exit 1
 fi
 
+FAILED=0
+
 for cfg in /etc/juniper-address-set-mapping/*; do
     echo "Processing $cfg ..."
 
     set +e
     JUNIPER_ADDRESS_SET="$(basename $cfg)" IPS_SOURCE_URL="$(cat $cfg)" $BIN
+
+    if [ $? -ne 0 ]; then
+        let FAILED++
+    fi
     set -e
 done
 
+echo "Failed processings: $FAILED"
 echo "DONE."
+
+exit $FAILED
